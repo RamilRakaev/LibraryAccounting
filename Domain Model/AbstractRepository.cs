ba3f6f7a6865121fdd47aa887fe103b8ea658287
RamilRakaev <59777361+RamilRakaev@users.noreply.github.com>
2021-08-12 21:29:30 +0300

    Добавьте файлы проекта.

---
+++ Domain Model/AbstractRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DomainLayer.DomainRepository
+{
+    public interface AbstractRepository<Element>
+    {
+        Element Find(int id);
+
+        void Save();
+    }
+}
