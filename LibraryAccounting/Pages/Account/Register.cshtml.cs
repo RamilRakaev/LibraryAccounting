6445252f60a5e748cb0b7f9a927601ab542bc788
RamilRakaev <59777361+RamilRakaev@users.noreply.github.com>
2021-09-12 20:26:49 +0300

    Добавлено подтверждение почты при регистрации (не отлажено).

--- LibraryAccounting/Pages/Account/Register.cshtml.cs
+++ LibraryAccounting/Pages/Account/Register.cshtml.cs
@@ -1,7 +1,10 @@
 
 using System.Linq;
 using System.Security.Claims;
+using System.Threading;
 using System.Threading.Tasks;
+using Antlr.Runtime;
+using LibraryAccounting.CQRSInfrastructure.EmailManagement;
 using LibraryAccounting.Domain.Model;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -31,16 +34,45 @@ namespace LibraryAccounting.Pages.Account
 
         public async Task<IActionResult> OnPost(RegisterViewModel register)
         {
+            //if (ModelState.IsValid)
+            //{
+            //    ApplicationUser user = new() { UserName = register.Name, Email = register.Email, RoleId = 1 };
+            //    var result = await _userManager.CreateAsync(user, register.Password);
+            //    if (result.Succeeded)
+            //    {
+            //        await _signInManager.SignInAsync(user, false);
+            //        await _userManager.AddClaimAsync(user, new Claim("roleId", user.RoleId.ToString()));
+            //        await _userManager.UpdateAsync(user);
+            //        await _userManager.ConfirmEmailAsync(user);
+            //        return RedirectToPage("/ClientPages/BookCatalog");
+            //    }
+            //    else
+            //    {
+            //        foreach (var error in result.Errors)
+            //        {
+            //            ModelState.AddModelError(string.Empty, error.Description);
+            //        }
+            //    }
+            //}
             if (ModelState.IsValid)
             {
-                ApplicationUser user = new() { UserName = register.Name, Email = register.Email, RoleId = 1 };
+                ApplicationUser user = new() { UserName = register.Name, Email = register.Email, Password = register.Password, RoleId = 1};
+                // добавляем пользователя
                 var result = await _userManager.CreateAsync(user, register.Password);
                 if (result.Succeeded)
                 {
-                    await _signInManager.SignInAsync(user, false);
-                    _ = User.Claims.Append(new Claim("roleId", user.RoleId.ToString()));
-                    await _userManager.UpdateAsync(user);
-                    return RedirectToPage("/ClientPages/BookCatalog");
+                    // генерация токена для пользователя
+                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    var callbackUrl = Url.Action(
+                        "ConfirmEmail",
+                        "Register",
+                        new { userId = user.Id, code = code },
+                        protocol: HttpContext.Request.Scheme);
+                    MessageSending emailService = new MessageSending();
+                    await emailService.SendEmailAsync(register.Email, "Confirm your account",
+                        $"Подтвердите регистрацию, перейдя по ссылке: <a href='{callbackUrl}'>link</a>");
+
+                    return Content("Для завершения регистрации проверьте электронную почту и перейдите по ссылке, указанной в письме");
                 }
                 else
                 {
